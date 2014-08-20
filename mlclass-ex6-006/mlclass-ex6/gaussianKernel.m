129f92897073fc40b7070fc1c68f60a31a5e9ebe
dengmare <nightmare.deng@gmail.com>
2014-08-20 19:13:19 +0800

    material on 6th exercise
    no answers yet

--- mlclass-ex6-006/mlclass-ex6/gaussianKernel.m
+++ mlclass-ex6-006/mlclass-ex6/gaussianKernel.m
@@ -14,10 +14,9 @@ sim = 0;
 %               and x2 computed using a Gaussian kernel with bandwidth
 %               sigma
 %
-% size(x1) = [ 3, 1 ]
-% size(x2) = [ 3, 1 ]
+%
 
- sim = 1/exp(((x1 - x2)' * (x1 -x2))/(2 * sigma^2));
+
 
 
 
